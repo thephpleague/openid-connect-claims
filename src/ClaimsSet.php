1bb9e59d586204964534350f540e36adf18fccfd
Alex Bilbie <hello@alexbilbie.com>
2016-05-26 14:41:04 +0100

    First commit

---
+++ src/ClaimsSet.php
@@ -0,0 +1,263 @@
+<?php
+
+namespace League\OpenIdConnectClaims;
+
+class ClaimsSet implements ClaimsSetInterface
+{
+    private $attributes = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function setIdentifier($id)
+    {
+        $this->attributes['sub'] = (string) $id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setName($name)
+    {
+        $this->attributes['name'] = (string) $name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setFirstName($firstName)
+    {
+        $this->attributes['given_name'] = (string) $firstName;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setMiddleName($middleName)
+    {
+        $this->attributes['middle_name'] = (string) $middleName;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setLastName($lastName)
+    {
+        $this->attributes['family_name'] = (string) $lastName;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setNickname($nickname)
+    {
+        $this->attributes['nickname'] = (string) $nickname;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setUsername($username)
+    {
+        $this->attributes['preferred_username'] = (string) $username;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setProfileUrl($url)
+    {
+        $this->attributes['profile'] = (string) $url;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setPictureUrl($url)
+    {
+        $this->attributes['picture'] = (string) $url;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setWebsite($url)
+    {
+        $this->attributes['website'] = (string) $url;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setEmail($email)
+    {
+        $this->attributes['email'] = (string) $email;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setEmailVerified($verified = true)
+    {
+        $this->attributes['email_verified'] = (bool) $verified;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setGender($gender)
+    {
+        $this->attributes['gender'] = (string) $gender;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setBirthDate($year = '0000', $month = null, $day = null)
+    {
+        $this->attributes['birthdate'] = ($month !== null && $day !== null)
+            ? sprintf('%s-%s-%s', $year, $month, $day)
+            : $year;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setZoneInfo($zoneInfo)
+    {
+        $this->attributes['zoneinfo'] = (string) $zoneInfo;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setLocale($locale)
+    {
+        $this->attributes['locale'] = (string) $locale;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setPhoneNumber($phoneNumber, $extension = null)
+    {
+        $this->attributes['phone_number'] = ($extension !== null)
+            ? sprintf('%s;ext=%s', $phoneNumber, $extension)
+            : $phoneNumber;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setPhoneNumberVerified($verified = true)
+    {
+        $this->attributes['phone_number_verified'] = (bool) $verified;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAddressStreet($street)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['street_address'] = (string) $street;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAddressLocality($locality)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['locality'] = (string) $locality;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAddressPostalCode($postalCode)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['postal_code'] = (string) $postalCode;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAddressRegion($region)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['region'] = (string) $region;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAddressCountry($country)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['country'] = (string) $country;
+    }
+
+    /**
+     * Set the user's address as a formatted string
+     *
+     * @param string|null $formattedAddress    Full mailing address, formatted for display or use on a mailing list.
+     *                                         This field MAY contain multiple lines, separated by newlines. Newlines
+     *                                         can be represented either as a carriage return/line feed pair ("\r\n")
+     *                                         or as a single line feed character ("\n").
+     */
+    public function setFormattedAddress($formattedAddress)
+    {
+        if (array_key_exists('address', $this->attributes) === false) {
+            $this->attributes['address'] = [];
+        }
+
+        $this->attributes['address']['formatted'] = (string) $formattedAddress;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function jsonSerialize()
+    {
+        if (
+            array_key_exists('name', $this->attributes) === false
+            && (array_key_exists('given_name', $this->attributes)
+                || array_key_exists('family_name', $this->attributes)
+                || array_key_exists('middle_name', $this->attributes)
+            )
+        ) {
+            $attr = $this->attributes;
+            $attr['name'] = preg_replace(
+                '/\s+/',
+                ' ',
+                implode(
+                    ' ',
+                    [
+                        @$this->attributes['given_name'],
+                        @$this->attributes['middle_name'],
+                        @$this->attributes['family_name'],
+                    ]
+                )
+            );
+            return $attr;
+        }
+        return $this->attributes;
+    }
+}
---
+++ src/ClaimsSetInterface.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace League\OpenIdConnectClaims;
+
+interface ClaimsSetInterface extends \JsonSerializable
+{
+    /**
+     * Set the identifier for the end user
+     *
+     * @param string $id
+     */
+    public function setIdentifier($id);
+
+    /**
+     * Set the user's name
+     *
+     * @param string $name
+     */
+    public function setName($name);
+
+    /**
+     * Set the user's first name
+     *
+     * @param string $firstName
+     */
+    public function setFirstName($firstName);
+
+    /**
+     * Set the user's middle name
+     *
+     * @param string $middleName
+     */
+    public function setMiddleName($middleName);
+
+    /**
+     * Set the user's last name
+     *
+     * @param string $lastName
+     */
+    public function setLastName($lastName);
+
+    /**
+     * Set the user's nickname
+     *
+     * @param string $nickname
+     */
+    public function setNickname($nickname);
+
+    /**
+     * Set the user's username
+     *
+     * @param string $username
+     */
+    public function setUsername($username);
+
+    /**
+     * Set the URL to user's profile
+     *
+     * @param string $url
+     */
+    public function setProfileUrl($url);
+
+    /**
+     * Set the URL to the user's picture
+     *
+     * @param string $url
+     */
+    public function setPictureUrl($url);
+
+    /**
+     * Set the URL to the user's website
+     *
+     * @param string $url
+     */
+    public function setWebsite($url);
+
+    /**
+     * Set the user's email
+     *
+     * @param string $email
+     */
+    public function setEmail($email);
+
+    /**
+     * Set to true if the user's email has been verified
+     *
+     * @param bool $verified
+     */
+    public function setEmailVerified($verified = true);
+
+    /**
+     * Set the user's gender
+     *
+     * @param string $gender
+     */
+    public function setGender($gender);
+
+    /**
+     * Set the user's birth date
+     *
+     * @param string      $year Set the year to 0000 to not declare the year
+     * @param string|null $month
+     * @param string|null $day
+     */
+    public function setBirthDate($year = '0000', $month = null, $day = null);
+
+    /**
+     * Set the user's zoneinfo
+     *
+     * @param string $zoneInfo (e.g. Europe/London)
+     */
+    public function setZoneInfo($zoneInfo);
+
+    /**
+     * Set the user's locale
+     *
+     * @param string $locale
+     */
+    public function setLocale($locale);
+
+    /**
+     * Set the user's phone number
+     *
+     * @param string      $phoneNumber
+     * @param string|null $extension
+     */
+    public function setPhoneNumber($phoneNumber, $extension = null);
+
+    /**
+     * Set to true if the user's phone number has been verified
+     *
+     * @param bool $verified
+     */
+    public function setPhoneNumberVerified($verified = true);
+
+    /**
+     * Set the user's address street
+     *
+     * @param string $street  Full street address component, which MAY include house number, street name,
+     *                        Post Office Box, and multi-line extended street address information. This
+     *                        field MAY contain multiple lines, separated by newlines. Newlines can be
+     *                        represented either as a carriage return/line feed pair ("\r\n") or as a single
+     *                        line feed character ("\n").
+     */
+    public function setAddressStreet($street);
+
+    /**
+     * Set the user's address locality
+     *
+     * @param string $locality City or locality component
+     */
+    public function setAddressLocality($locality);
+
+    /**
+     * Set the user's address postal code
+     *
+     * @param string $postalCode Zip code or postal code component.
+     */
+    public function setAddressPostalCode($postalCode);
+
+    /**
+     * Set the user's address region
+     *
+     * @param string $region State, province, prefecture, or region component.
+     */
+    public function setAddressRegion($region);
+
+    /**
+     * Set the user's address country
+     *
+     * @param string $country Country name component.
+     */
+    public function setAddressCountry($country);
+
+    /**
+     * Set the user's address as a formatted string
+     *
+     * @param string|null $formattedAddress    Full mailing address, formatted for display or use on a mailing list.
+     *                                         This field MAY contain multiple lines, separated by newlines. Newlines
+     *                                         can be represented either as a carriage return/line feed pair ("\r\n")
+     *                                         or as a single line feed character ("\n").
+     */
+    public function setFormattedAddress($formattedAddress);
+}
